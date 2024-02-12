c5c65e5b5c5c197c3fab6330d9b74f72430c4bae
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-02-12 17:26:23 +0700

    commit

---
+++ resources/views/mahasiswa/create.blade.php
@@ -0,0 +1,33 @@
+        @extends('layouts.master')
+        @section('content')
+        <h1 class="text-center pt-3">Data Mahasiswa</h1>
+       <!-- START FORM -->
+       <form action='{{url('mahasiswa')}}' method='post'>
+        @csrf
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <div class="mb-3 row">
+                <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control" name='nim' id="nim" autocomplete="off">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name='nama' id="nama" autocomplete="off">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="jurusan" class="col-sm-2 col-form-label">Jurusan</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name='jurusan' id="jurusan" autocomplete="off">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="jurusan" class="col-sm-2 col-form-label"></label>
+                <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button></div>
+            </div>
+        </div>
+    </form>
+    <!-- AKHIR FORM -->
+    @endsection
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,74 @@
+        
+        @extends('layouts.master')
+        @section('content')
+        <h1 class="text-center pt-3">Data Mahasiswa</h1>
+       <!-- START FORM -->
+       <form action='' method='post'>
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <div class="mb-3 row">
+                <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control" name='nim' id="nim" autocomplete="off">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name='nama' id="nama" autocomplete="off">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="jurusan" class="col-sm-2 col-form-label">Jurusan</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name='jurusan' id="jurusan" autocomplete="off">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="jurusan" class="col-sm-2 col-form-label"></label>
+                <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button></div>
+            </div>
+          </form>
+        </div>
+        <!-- AKHIR FORM -->
+        
+        <!-- START DATA -->
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+                <!-- FORM PENCARIAN -->
+                <div class="pb-3">
+                  <form class="d-flex" action="" method="get">
+                      <input class="form-control me-1" type="search" name="katakunci" value="{{ Request::get('katakunci') }}" placeholder="Masukkan kata kunci" aria-label="Search">
+                      <button class="btn btn-secondary" type="submit" autocomplete="off">Cari</button>
+                  </form>
+                </div>
+                
+                <!-- TOMBOL TAMBAH DATA -->
+                <div class="pb-3">
+                  <a href='' class="btn btn-primary">+ Tambah Data</a>
+                </div>
+          
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th class="col-md-1">No</th>
+                            <th class="col-md-3">NIM</th>
+                            <th class="col-md-4">Nama</th>
+                            <th class="col-md-2">Jurusan</th>
+                            <th class="col-md-2">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>220535601431</td>
+                            <td>Ammar</td>
+                            <td>Teknik Informatika</td>
+                            <td>
+                                <a href='' class="btn btn-success btn-sm">Ubah</a>
+                                <a href='' class="btn btn-danger btn-sm">Hapus</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+          </div>
+          <!-- AKHIR DATA -->
+          @endsection
